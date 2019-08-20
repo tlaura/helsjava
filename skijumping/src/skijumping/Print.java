482c5db8b19ed1d278359ddab5c5cb4aad325906
Laura <tundelaura93@gmail.com>
2019-08-20 12:19:20 -0400

    print start ski jumping

---
+++ skijumping/src/skijumping/Print.java
@@ -0,0 +1,18 @@
+package skijumping;
+
+public class Print {
+
+    public static void start(){
+        System.out.println("Kumpula ski jumping week\n");
+
+        System.out.println("Write the names of the participants one at a time; " +
+                "an empty string brings you to the jumping phase.\n");
+    }
+
+    public static void askForNames(){
+
+
+
+    }
+
+}
